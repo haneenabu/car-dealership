ef549e09bec41f2e418af95e2ff38146ac2bed36
Haneen Abu-Khater <abukhater.haneen@gmail.com>
2017-07-30 21:16:46 -0700

    add functionality to add a car in UI

--- src/main/java/models/App.java
+++ src/main/java/models/App.java
@@ -4,43 +4,83 @@ import models.Vehicle;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("What is your maximum budget for a vehicle?");
+        Vehicle hatchback = new Vehicle(1994, "Subaru", "Legacy", 170000, 4000);
+        Vehicle suv = new Vehicle(2002, "Ford", "Explorer", 100000, 7000);
+        Vehicle sedan = new Vehicle(2015, "Toyota", "Camry", 50000, 30000);
+        Vehicle truck = new Vehicle(1999, "Ford", "Ranger", 100000, 4000);
+        Vehicle crossover = new Vehicle(1998, "Toyota", "Rav-4", 200000, 3500);
 
-        try {
-            String stringUserMaxBudget = bufferedReader.readLine();
+        ArrayList<Vehicle> allVehicles = new ArrayList<Vehicle>();
 
-            Vehicle hatchback = new Vehicle(1994, "Subaru", "Legacy", 170000, 4000);
-            Vehicle suv = new Vehicle(2002, "Ford", "Explorer", 100000, 7000);
-            Vehicle sedan = new Vehicle(2015, "Toyota", "Camry", 50000, 30000);
-            Vehicle truck = new Vehicle(1999, "Ford", "Ranger", 100000, 4000);
-            Vehicle crossover = new Vehicle(1998, "Toyota", "Rav-4", 200000, 3500);
+        allVehicles.add(hatchback);
+        allVehicles.add(suv);
+        allVehicles.add(sedan);
+        allVehicles.add(truck);
+        allVehicles.add(crossover);
+        while (true) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Welcome to our car dealership. What would you like to do? Enter one of the following options: All Vehicles, Search Price, or Add Vehicle");
 
-            Vehicle[] allVehicles = {hatchback, suv, sedan, truck, crossover};
+            try {
 
-            int userMaxBudget = Integer.parseInt(stringUserMaxBudget);
+                String navigationChoice = bufferedReader.readLine();
 
-            System.out.println("Alright, here's what we have in your price range:");
+                if (navigationChoice.equals("All Vehicles")) {
+                    for (Vehicle individualVehicle : allVehicles) {
+                        System.out.println("----------------------");
+                        System.out.println(individualVehicle.year);
+                        System.out.println(individualVehicle.brand);
+                        System.out.println(individualVehicle.model);
+                        System.out.println(individualVehicle.miles);
+                        System.out.println(individualVehicle.price);
+                    }
+                } else if (navigationChoice.equals("Search Price")) {
+                    System.out.println("What is your maximum budget for a vehicle?");
+                    String stringUserMaxBudget = bufferedReader.readLine();
+                    int userMaxBudget = Integer.parseInt(stringUserMaxBudget);
+                    System.out.println("Alright, here's what we have in your price range:");
+                    for (Vehicle individualVehicle : allVehicles) {
+                        if (individualVehicle.worthBuying(userMaxBudget)) {
+                            System.out.println("----------------------");
+                            System.out.println(individualVehicle.year);
+                            System.out.println(individualVehicle.brand);
+                            System.out.println(individualVehicle.model);
+                            System.out.println(individualVehicle.miles);
+                            System.out.println(individualVehicle.price);
+                        }
+                    }
+                } else if (navigationChoice.equals("Add Vehicle")) {
+                    System.out.println("Alright, let's add a vehicle! What year was this vehicle made?");
+                    int userVehicleYear = Integer.parseInt(bufferedReader.readLine());
+                    System.out.println("Great! What make or brand is the vehicle?");
+                    String userVehicleBrand = bufferedReader.readLine();
+                    System.out.println("Got it! What model is it?");
+                    String userVehicleModel = bufferedReader.readLine();
+                    System.out.println("And how many miles does it have on it?");
+                    int userVehicleMiles = Integer.parseInt(bufferedReader.readLine());
+                    System.out.println("Finally, what's its price?");
+                    int userVehiclePrice = Integer.parseInt(bufferedReader.readLine());
+                    Vehicle userVehicle = new Vehicle(userVehicleYear, userVehicleBrand, userVehicleModel, userVehicleMiles, userVehiclePrice);
+                    allVehicles.add(userVehicle); //I'm new!
 
-            for (Vehicle individualVehicle : allVehicles) {
-                if (individualVehicle.worthBuying(userMaxBudget)) {
+                    System.out.println("Alright, here's your new vehicle:");
                     System.out.println("----------------------");
-                    System.out.println(individualVehicle.year);
-                    System.out.println(individualVehicle.brand);
-                    System.out.println(individualVehicle.model);
-                    System.out.println(individualVehicle.miles);
-                    System.out.println(individualVehicle.price);
+                    System.out.println(userVehicle.year);
+                    System.out.println(userVehicle.brand);
+                    System.out.println(userVehicle.model);
+                    System.out.println(userVehicle.miles);
+                    System.out.println(userVehicle.price);
+                } else {
+                    System.out.println("I'm sorry, we don't recognize your input");
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        catch(IOException e)
-        {
-            e.printStackTrace();
-        }
     }
-
 }
